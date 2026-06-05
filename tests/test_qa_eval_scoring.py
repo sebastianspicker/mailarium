46b93decadadf98dbb30c89d291640caea6079fb
Sebastian Spicker <sebastian.spicker@googlemail.com>
2026-06-05 11:00:07 +0200

    fix: align local codacy preflight

--- tests/test_qa_eval_scoring.py
+++ tests/test_qa_eval_scoring.py
@@ -1,5 +1,14 @@
-# pylint: disable=unused-wildcard-import,wildcard-import
+from types import ModuleType
 
-from ._qa_eval_scoring_attachment_cases import *  # noqa: F403
-from ._qa_eval_scoring_core_cases import *  # noqa: F403
-from ._qa_eval_scoring_tail_cases import *  # noqa: F403
+from . import _qa_eval_scoring_attachment_cases, _qa_eval_scoring_core_cases, _qa_eval_scoring_tail_cases
+
+
+def _export_tests(module: ModuleType) -> None:
+    for name in dir(module):
+        if name.startswith("test_"):
+            globals()[name] = getattr(module, name)
+
+
+_export_tests(_qa_eval_scoring_attachment_cases)
+_export_tests(_qa_eval_scoring_core_cases)
+_export_tests(_qa_eval_scoring_tail_cases)
